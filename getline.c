3ebe512e6ff2548594ecd5a9635f7220ecd65a70
jzaval17 <jzaval17@calpoly.edu>
2024-02-06 19:08:23 -0800

    Initial

---
+++ getline.c
@@ -0,0 +1,44 @@
+#define _GNU_SOURCE // Enables GNU-specific extensions, getline() is one of them
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // Include for free
+#include <errno.h> // Include for errno and perror
+
+
+int main(int argc, char * argv[]) {
+    FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    char *last = NULL, *secondLast = NULL;
+
+    if (argc != 2){ //if the number of arguments is not 2, print an error message
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+
+    }
+    file = fopen(argv[1], "r");// Attempt to open file
+    if (file == NULL) {//if the file does not exist, print an error message
+        fprintf(stderr, "Error: %s: Unexpected setback\n", argv[1]);
+        perror("");
+        return 1;
+    }
+    //the while loop reads the file line by line and stores the second last and last line
+    while (getline(&line, &len, file) != -1) {// Read the file line by line
+        free(secondLast);
+        secondLast = last;// Store the last line
+        last = strdup(line);// Duplicate the current line
+        }
+        if (secondLast != NULL) {
+            printf("%s", secondLast);
+            free(secondLast);
+        }
+        if (last != NULL) {
+            printf("%s", last);
+            free(last);
+        }
+        
+        free(line);
+        fclose(file);
+        return 0;
+    }
+
