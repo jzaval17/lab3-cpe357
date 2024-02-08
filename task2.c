fd4243b350b2e001dc52ffaedece1f5720393d85
jzaval17 <jzaval17@calpoly.edu>
2024-02-08 13:13:27 -0800

    2048

--- task2.c
+++ task2.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 2048
 int main(){
     char buffer[BUFFER_SIZE];
     FILE *file = fopen("10mb.txt", O_RDONLY);
