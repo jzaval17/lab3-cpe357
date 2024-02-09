213a446a33778a2c1d951c124baa942ac082ce76
jzaval17 <jzaval17@calpoly.edu>
2024-02-09 11:47:13 -0800

    last line

--- task2.c
+++ task2.c
@@ -3,21 +3,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUFFER_SIZE 1
-int main(int argc, char *argv[]) {
+#define BUFFER_SIZE 8192
+int main(){
     char buffer[BUFFER_SIZE];
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+    FILE *file = fopen("10mb.txt", O_RDONLY);
+    if (file < 0) { //if the file does not exist, print an error message
+        perror("Error opening %s");
         return 1;
     }
-    int file = open(argv[1], O_RDONLY);
-    if (file < 0) { // If the file does not exist, print an error message including the file name
-        fprintf(stderr, "Error opening %s\n", argv[1]);
-        return 1;
+    while (fread(buffer, 1, BUFFER_SIZE, file) > 0) {// Read the file character by character
+    //wont print anything
     }
-    while (read(file, buffer, BUFFER_SIZE) > 0) {
-        // Process buffer here if needed
-    }
-    close(file);
+    fclose(file);
     return 0;
+
 }
